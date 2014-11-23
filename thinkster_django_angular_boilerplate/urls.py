26c88928e7dd3ba53ba3c0b3d173355508ff2004
dmartinezayuso <diego.martinez.ayuso@gmail.com>
2014-11-23 23:37:55 +0100

    logout not working, in progress

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, url, include
 from django.views.generic.base import TemplateView
 from rest_framework import routers
-from authentication.views import AccountViewSet
+from authentication.views import AccountViewSet, LoginView, LogoutView
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
-    '',    
+    '',
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),    
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
     url(r'^api/v1/', include(router.urls)),
     url(r'^', TemplateView.as_view(template_name='index.html')),
 )
